a1a3338ab73dc847786a2d8606200aa3daf8aeb9
Viacheslav <78733510+Slavik-s-line@users.noreply.github.com>
2022-05-08 17:26:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,21 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 
+
 df = pd.read_csv("train.csv")
 df2 = pd.read_csv("test.csv")
-
-df[list(pd.get_dummies(df['education_form']).columns)] = pd.get_dummies(df['education_form'])
-df2[list(pd.get_dummies(df['education_form']).columns)] = pd.get_dummies(df['education_form'])
-
-df['has_mobile'] = df['has_mobile'].apply(int)
-df2['has_mobile'] = df2['has_mobile'].apply(int)
-
-to_del = ['bdate', 'followers_count', 'graduation', 'education_form',
+to_del = ['bdate', 'followers_count', 'graduation',
           'relation', 'life_main', 'people_main', 'city', 'last_seen',
           'occupation_name', 'career_start', 'career_end']
 
 df.drop(to_del, axis=1, inplace=True)
 df2.drop(to_del, axis=1, inplace=True)
 
-# ---------------------------------------------------------------------
+df["has_mobile"] = df["has_mobile"].apply(int)
+df2["has_mobile"] = df2["has_mobile"].apply(int)
 
+
+# ------------------------------------------------------
 
 def fill_sex(sex):
     if sex == 2:
@@ -30,8 +27,8 @@ def fill_sex(sex):
 df['sex'] = df['sex'].apply(fill_sex)
 df2['sex'] = df2['sex'].apply(fill_sex)
 
-# ---------------------------------------------------------------------
 
+# ------------------------------------------------------
 
 def education(status):
     if status == "Undergraduate applicant":
@@ -55,28 +52,8 @@ def education(status):
 df['education_status'] = df['education_status'].apply(education)
 df2['education_status'] = df2['education_status'].apply(education)
 
-# ---------------------------------------------------------------------
 
-
-def list_of_langs(langs):
-    if len(langs.split(";")) == 1:
-        return "1 lang"
-    elif len(langs.split(";")) == 2:
-        return "2 lang"
-    else:
-        return "More 3 lang"
-
-
-df['langs'] = df['langs'].apply(list_of_langs)
-df2['langs'] = df2['langs'].apply(list_of_langs)
-df[list(pd.get_dummies(df['langs']).columns)] = pd.get_dummies(df['langs'])
-df2[list(pd.get_dummies(df['langs']).columns)] = pd.get_dummies(df['langs'])
-df.drop('langs', axis=1, inplace=True)
-df2.drop('langs', axis=1, inplace=True)
-df.info()
-
-# ---------------------------------------------------------------------
-
+# ------------------------------------------------------
 
 def occupation(type):
     if type == "university":
@@ -86,9 +63,35 @@ def occupation(type):
 
 df['occupation_type'] = df['occupation_type'].apply(occupation)
 df2['occupation_type'] = df2['occupation_type'].apply(occupation)
-df.info()
 
-# ---------------------------------------------------------------------
+# ------------------------------------------------------
+
+df[list(pd.get_dummies(df["education_form"]).columns)] = pd.get_dummies(df["education_form"])
+df2[list(pd.get_dummies(df2["education_form"]).columns)] = pd.get_dummies(df2["education_form"])
+df.drop("education_form", axis=1, inplace=True)
+df2.drop("education_form", axis=1, inplace=True)
+# ------------------------------------------------------
+
+
+def list_of_langs(langs):
+    if len(langs.split(";")) == 1:
+        return "1 lang"
+    elif len(langs.split(";")) == 2:
+        return "2 lang"
+    else:
+        return "More than 3 lang"
+
+
+df['langs'] = df['langs'].apply(list_of_langs)
+df2['langs'] = df2['langs'].apply(list_of_langs)
+
+df[list(pd.get_dummies(df["langs"]).columns)] = pd.get_dummies(df["langs"])
+df2[list(pd.get_dummies(df2["langs"]).columns)] = pd.get_dummies(df2["langs"])
+
+df.drop("langs", axis=1, inplace=True)
+df2.drop("langs", axis=1, inplace=True)
+df.info()
+# ------------------------------------------------------
 
 X_train = df.drop('result', axis=1)
 y_train = df['result']
